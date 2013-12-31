ec0adc553b73175085169ea60e7637dc7862cbd2
Alistair Smith <alistairsmith@flightofstairs.org>
2013-12-31 20:20:45 +0000

    Adding footer.

---
+++ app/views/constants.scala
@@ -0,0 +1,11 @@
+package views
+
+
+
+object constants {
+  val links = Seq(
+    ("GitHub", "https://github.com/FlightOfStairs"),
+    ("LinkedIn", "http://www.linkedin.com/in/alistairfsmith"),
+    ("Twitter", "https://twitter.com/FOfStairs")
+  )
+}
