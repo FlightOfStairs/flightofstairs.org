140f7f22564ab3e2e6e6cdc89fe6d0db61a64c3f
Alistair Smith <alistairsmith@flightofstairs.org>
2014-01-20 22:35:34 +0000

    Adding font awesome icons.

--- app/models/constants.scala
+++ app/models/constants.scala
@@ -1,9 +1,10 @@
 package models
 
 object constants {
+
   val links = Seq(
-    ("GitHub", "https://github.com/FlightOfStairs"),
-    ("LinkedIn", "http://www.linkedin.com/in/alistairfsmith"),
-    ("Twitter", "https://twitter.com/FOfStairs")
+    ("GitHub", "https://github.com/FlightOfStairs", "github"),
+    ("LinkedIn", "http://www.linkedin.com/in/alistairfsmith", "linkedin"),
+    ("Twitter", "https://twitter.com/FOfStairs", "twitter")
   )
 }
--- build.sbt
+++ build.sbt
@@ -6,7 +6,8 @@ libraryDependencies ++= Seq(
   cache,
   "org.webjars" %% "webjars-play" % "2.2.0",
   "org.webjars" % "jquery" % "2.0.3",
-  "org.webjars" % "bootstrap" % "3.0.2"
+  "org.webjars" % "bootstrap" % "3.0.2",
+  "org.webjars" % "font-awesome" % "4.0.3"
 )
 
 play.Project.playScalaSettings
