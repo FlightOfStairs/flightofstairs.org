bb44fb845b098aa0b3139612a47f413670e7248d
Alistair Smith <alistairsmith@flightofstairs.org>
2014-01-26 19:32:26 +0000

    Adding more likes.

--- app/models/Likes.scala
+++ app/models/Likes.scala
@@ -11,6 +11,11 @@ object Likes {
         Like("Duck typing",  "…for small, single-developer projects and prototypes. If the code does fit in one brain, is not tightly coupled, and is rapidly changing, why should I care that the compiler isn't sure what's going on? I enjoy using Clojure and Groovy for scripting; with optional types now available in each, there's little downside."))),
 
     LikeCategory("Space", "I am very interested in space and access to space. I watch most rocket launches.", List(
-      Like("SpaceX", "I am a fan of SpaceX and Elon Musk's companies in general. I'm excited about getting to see the two Dragon abort tests this year, along with the Falcon Heavy (first HLV with fuel crossfeed) maiden flight and progress towards recovering Falcon cores in the near future."))
-    ))
+      Like("SpaceX", "I am a fan of SpaceX and Elon Musk's companies in general. I'm excited about getting to see the two Dragon abort tests this year, along with the Falcon Heavy (first HLV with fuel crossfeed) maiden flight and progress towards recovering Falcon cores in the near future."),
+      Like("Dream Chaser", "The coolest of the spacecraft being developed for NASA's commercial crew program. I hope one day to see SpaceX delivering this to orbit."))),
+
+    LikeCategory("Games", "I'm pretty keen on playing games, particularly those that are single-player and story-driven.", List(
+      Like("The Witcher 2", "Very impressed with this game. It's got a great story, good mechanics, good replayability and surprisingly good graphics. I'd recommend it to anyone who had 40 hours to kill.")))
+
+  )
 }
