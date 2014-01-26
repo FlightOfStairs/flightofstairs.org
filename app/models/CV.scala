11404769edbcdaf2e57db4c84e5e61a096a9c3b0
Alistair Smith <alistairsmith@flightofstairs.org>
2014-01-26 15:55:25 +0000

    Updating CV.

--- app/models/CV.scala
+++ app/models/CV.scala
@@ -6,26 +6,31 @@ import play.api.i18n.Messages
 
 case class CV(name: String, email: String, phone: String, skills: List[SkillArea], employment: List[Work], uni: Education)
 case class SkillArea(area: String, details: String)
-case class Work(org: String, title: String, from: YearMonth, to: Option[YearMonth], description: String)
+case class Work(org: String, title: String, from: YearMonth, to: Option[YearMonth], details: List[String])
 case class Education(university: Work, awards: List[String])
 
 
-object CV extends CV("Alistair Smith", Messages("email"), "07512853460",
+object CV extends CV("Alistair Smith", Messages("email"), "+447512853460",
     List(
       SkillArea("Familiar languages", "Java, Scala, R, HTML, JS, CSS"),
       SkillArea("Previous experience", "Groovy, C#, some Lisps, SQL, Haskell, Python, C"),
-      SkillArea("Design", "Distributed systems, managing big data, service oriented architecture"),
+      SkillArea("Design", "Distributed systems, managing big data, Service Oriented Architecture"),
       SkillArea("Systems", "Various AWS products, LAMP stack, continuous integration/deployment, IaaS, PaaS"),
       SkillArea("Other", "VC (Git, SVN, Others), Agile methodologies")),
     List(
-      Work("Amazon", "Software Development Engineer", new YearMonth(2012, 7), None, "Worked on a team responsible for real-time services used by Amazon. Agile practises are widely used."),
+      Work("Amazon.com", "Software Development Engineer", new YearMonth(2012, 7), None, List(
+        "My current team is responsible for a customer-facing service with many performance and scalability challenges. I've learned to build and manage distributed systems and service oriented architectures to satisfy very low latency requirements. I've worked with big data analysis, in both an ongoing, systematic approach, as well as answering ad-hoc questions to drive specific business decisions.",
+      "I've worked on innovative projects and prototypes in small sub-teams to open up new areas of business for Amazon, of which the best example is yet to be announced. I have a strong desire for high standards and there are many occasions where I've improved and simplified my team's world, either in my own time or by getting buy-in from other stakeholders.",
+      "In addition to my primary responsibilities, I've been involved with other aspects of Amazon. I am very involved with recruitment and interviewing for the centre. I've taken ownership of several projects that are shared across teams in the centre, including externally facing websites and an app used to teach early secondary school children the basics of programming.")),
 
-      Work("Defence Science and Technology Laboratory", "Software Engineer", new YearMonth(2010, 8), Some(new YearMonth(2011, 7)), "Worked as a member of a small software department. I had a great deal of freedom to design and implement solutions as I chose. Technologies I used included C#, Java, Python, Haskell, Evolutionary computation, and Trusted Computing. Areas of work included analysis of large datasets, creating tools to work with program disassembles, and recovering missing data through stochastic methods."),
+      Work("Defence Science and Technology Laboratory", "Software Engineer (intern)", new YearMonth(2010, 8), Some(new YearMonth(2011, 7)), List(
+        "Worked as a member of a small software department. I had a great deal of freedom to design and implement solutions as I chose. Technologies I used included C#, Java, Python, Haskell, Evolutionary computation, and Trusted Computing. Areas of work included analysis of “big data”, creating tools to work with program disassembles, and prediction of missing data.")),
 
-      Work("University of Strathclyde", "Research Intern", new YearMonth(2009, 6), Some(new YearMonth(2009, 9)), "The subject was \"Capturing Task Related Knowledge.\" I worked largely independently to explore ways of finding other resources relevant to the set of resources currently being used. I learned and gained experience on how to manage a medium-sized software project from start to finish.")
+      Work("University of Strathclyde", "Research Intern", new YearMonth(2009, 6), Some(new YearMonth(2009, 9)), List(
+        "The subject was “Capturing Task Related Knowledge.” I worked independently to explore ways of finding other resources relevant to the set of resources currently being used. I learned and gained experience on how to manage a medium-sized software project from start to finish."))
     ),
-    Education(Work("University of Strathclyde", "Software Engineering", new YearMonth(2007, 9), Some(new YearMonth(2012, 6)), "Secured a 10-week paid research internship over the summer 2009, have demonstrated my coursework to university applicants at open days in 2nd, 3rd and 4th years, and was a lab demonstrator for 1st and 3rd year classes."),
-
+    Education(Work("University of Strathclyde", "Software Engineering", new YearMonth(2007, 9), Some(new YearMonth(2012, 6)), List(
+      "Secured a 10-week paid research internship over the summer 2009, have demonstrated my coursework to university applicants at open days in 2nd, 3rd and 4th years, and was a lab demonstrator for 1st and 3rd year classes.")),
       List(
         "BSc Hons. 1st class",
         "Young Software Engineer of the Year (Scotland) &ndash; Best Engineered Project",
