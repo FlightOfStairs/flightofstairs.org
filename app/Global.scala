a9885dab4fb72500d394e3e51755371718d42e65
Alistair Smith <github@flightofstairs.org>
2015-04-06 13:43:08 +0100

    Setting X-Clacks-Overhead

--- app/Global.scala
+++ app/Global.scala
@@ -3,5 +3,9 @@ import play.api.mvc._
 import play.filters.gzip.GzipFilter
 
 object Global extends WithFilters(new GzipFilter()) with GlobalSettings {
-
+  override def doFilter(action: EssentialAction): EssentialAction = EssentialAction { request =>
+    action.apply(request).map(_.withHeaders(
+      "X-Clacks-Overhead" -> "GNU Terry Pratchett"
+    ))
+  }
 }
