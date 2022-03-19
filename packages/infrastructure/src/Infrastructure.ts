9d7b585a79155c01e727724b77699581a2289a14
Alistair Smith <alistairsmith@flightofstairs.org>
2022-03-19 18:41:54 +0000

    Add CF distribution

--- packages/infrastructure/src/Infrastructure.ts
+++ packages/infrastructure/src/Infrastructure.ts
@@ -1,5 +1,13 @@
 import { App, RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
-import { Bucket } from "aws-cdk-lib/aws-s3";
+import {
+  CloudFrontWebDistribution,
+  OriginAccessIdentity,
+} from "aws-cdk-lib/aws-cloudfront";
+import {
+  BlockPublicAccess,
+  Bucket,
+  BucketEncryption,
+} from "aws-cdk-lib/aws-s3";
 import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
 import { Construct } from "constructs";
 import { dirname } from "path";
@@ -8,16 +16,38 @@ export class Infrastructure extends Stack {
   constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
 
-    const destinationBucket = new Bucket(this, "WebsiteBucket", {
+    const websiteBucket = new Bucket(this, "WebsiteBucket", {
       autoDeleteObjects: true,
       removalPolicy: RemovalPolicy.DESTROY,
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+      encryption: BucketEncryption.S3_MANAGED,
     });
 
+    const originAccessIdentity = new OriginAccessIdentity(this, "OIA");
+    websiteBucket.grantRead(originAccessIdentity);
+
+    const distribution = new CloudFrontWebDistribution(
+      this,
+      "CloudFrontDistribution",
+      {
+        originConfigs: [
+          {
+            s3OriginSource: {
+              s3BucketSource: websiteBucket,
+              originAccessIdentity,
+            },
+            behaviors: [{ isDefaultBehavior: true }],
+          },
+        ],
+      }
+    );
+
     new BucketDeployment(this, "BucketDeployment", {
-      destinationBucket,
+      destinationBucket: websiteBucket,
       sources: [
         Source.asset(dirname(require.resolve("flightofstairs-website"))),
       ],
+      distribution,
     });
   }
 }
