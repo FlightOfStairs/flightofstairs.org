3c6fff4feee51c0aaddf04fe3fd673d78e486bb8
Alistair Smith <alistairsmith@flightofstairs.org>
2022-04-02 19:08:51 +0100

    CV Content Draft

--- packages/website/src/App.tsx
+++ packages/website/src/App.tsx
@@ -48,7 +48,7 @@ function App() {
       </AppBar>
       <Card>
         <CardContent>
-          <Typography variant="h5" component="h2">
+          <Typography variant="h4" component="h2">
             Skills
           </Typography>
 
@@ -84,121 +84,202 @@ function App() {
       </Card>
       <Card>
         <CardContent>
-          <Typography variant="h5" component="h2">
+          <Typography variant="h4" component="h2">
             Employment History
           </Typography>
 
           <Box>
-            <Typography variant="h6" component="h3">
+            <Typography variant="h5" component="h3">
               Amazon: Software Development
             </Typography>
-            <Typography variant="subtitle1">2011 to current date</Typography>
-            <p>
-              <Typography variant="body1">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </Typography>
-            </p>
-            <p>
-              <Typography variant="body1">
-                Teams and business areas (recent first)
-              </Typography>
-            </p>
-            <List>
-              <ListItem>
-                <ListItemText
-                  primary="Ivy"
-                  secondary="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                  eiusmod tempor incididunt ut labore et dolore magna aliqua."
-                />
-              </ListItem>
-              <ListItem>
-                <ListItemText
-                  primary="Noble"
-                  secondary="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                  eiusmod tempor incididunt ut labore et dolore magna aliqua."
-                />
-              </ListItem>
-              <ListItem>
-                <ListItemText
-                  primary="Amazon Registry"
-                  secondary="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                  eiusmod tempor incididunt ut labore et dolore magna aliqua."
-                />
-              </ListItem>
-              <ListItem>
-                <ListItemText
-                  primary="Amazon Registry"
-                  secondary="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                  eiusmod tempor incididunt ut labore et dolore magna aliqua."
-                />
-              </ListItem>
-              <ListItem>
-                <ListItemText
-                  primary="Talent Management"
-                  secondary="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                  eiusmod tempor incididunt ut labore et dolore magna aliqua."
-                />
-              </ListItem>
-              <ListItem>
-                <ListItemText
-                  primary="Talent Analytics"
-                  secondary="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                  eiusmod tempor incididunt ut labore et dolore magna aliqua."
-                />
-              </ListItem>
-              <ListItem>
-                <ListItemText
-                  primary="Dynamic Retail Ads"
-                  secondary="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                  eiusmod tempor incididunt ut labore et dolore magna aliqua."
-                />
-              </ListItem>
-            </List>
+            <Typography variant="subtitle1" component="span">
+              2011 to current date
+            </Typography>
+
+            <Typography variant="body1">
+              <p>
+                Significant experience delivering value to customers and
+                stakeholders across a wide range of Amazon's business areas: HR
+                Applications, HR Analytics, Amazon Registry Services
+                (registering gTLDs on customers' behalf), Advertising Analytics,
+                and Real-time auctions for online advertising.
+              </p>
+              <p>Cross-cutting responsibilities and achievements:</p>
+
+              <ul>
+                <li>
+                  Amazon Bar Raiser: Conducted 900+ technical and behavioural
+                  interviews. Mentored multiple Bar Raisers individually through
+                  6+ month training process. Supported and mentored Amazon's
+                  Early Insights program in its early years.
+                </li>
+                <li>
+                  Responsible for multiple security assessments and compliance
+                  for applications of all levels.
+                </li>
+                <li>
+                  Mentored multiple engineers through their promotion processes.
+                </li>
+                <li>
+                  Recipient of 3 patents for work on Real-time Advertising and
+                  Video Compositing systems.
+                </li>
+              </ul>
+            </Typography>
+
+            <Typography variant="h6" component="h4">
+              Teams and business areas
+            </Typography>
+            <Typography variant="subtitle1" component="span">
+              Recent first
+            </Typography>
+
+            <Typography variant="body1">
+              Ivy
+              <ul>
+                <li>
+                  Currently leading design of an HR-wide experimentation
+                  platform, enabling teams to understand how their changes
+                  impact employees, while enforcing legal, policy, and ethical
+                  constraints.
+                </li>
+                <li>
+                  Designed and implemented multiple applications with complex
+                  Infrastructure as a Service (IaaS) stacks.
+                </li>
+                <li>
+                  Led deep-dive investigation into latency of our applications,
+                  producing action-items with clear cost and impact estimates
+                  that have led to incremental investment of 2.5 engineers.
+                </li>
+                <li>
+                  Built stable, reactive, and self-healing search platform for
+                  HR content with ElasticSearch, respecting users' locale and
+                  access roles.
+                </li>
+                <li>
+                  Led urgent response to security event, conducting
+                  investigation, communication, and follow-up actions. Impact
+                  was seen at SVP-level.
+                </li>
+              </ul>
+            </Typography>
+
+            <Typography variant="body1" paragraph>
+              Noble
+              <ul>
+                <li>
+                  Designed and implemented an innovative data architecture
+                  merging batch-processed bulk updates with interactive user
+                  changes, allowing for a stable, self-healing, notification
+                  datastore.
+                </li>
+                <li>
+                  Proposed and conducted ridealong exercises with analyst
+                  partner team, giving insight into how we could support their
+                  needs, and influencing how we structured our working
+                  relationship.
+                </li>
+              </ul>
+            </Typography>
+
+            <Typography variant="body1" paragraph>
+              Amazon Registry
+              <ul>
+                <li>
+                  Implemented bot-validation workflows to ensure eligibility of
+                  customers of Amazon Registry's .bot domain.
+                </li>
+              </ul>
+            </Typography>
+
+            <Typography variant="body1" paragraph>
+              Talent Management
+              <ul>
+                <li>
+                  Participated in rapid delivery of multiple new HR applications
+                  supporting Annual Reviews, Promotions, and others.
+                </li>
+                <li>
+                  Led design and implementation of attachment and on-line
+                  analytics microservices.
+                </li>
+              </ul>
+            </Typography>
+
+            <Typography variant="body1" paragraph>
+              Talent Analytics
+              <ul>
+                <li>
+                  Proposed, designed, and led delivery of Sandcastles, a
+                  web-based analytics platform that enabled users to create
+                  ephemeral Spark clusters in order to work with Amazon's HR
+                  data in a secure environment.
+                </li>
+              </ul>
+            </Typography>
+
+            <Typography variant="body1" paragraph>
+              Dynamic Retail Ads
+              <ul>
+                <li>
+                  Individually designed and developed a real-time video
+                  compositing system to enable product images and information to
+                  be embedded within video ads.
+                </li>
+                <li>
+                  Migrated large on-host targetting datasets onto distributed
+                  Redis fleet while maintaining very low response times,
+                  allowing additional data to be considered and unlocking future
+                  experimentation in machine learning.
+                </li>
+                <li>
+                  Productionized team's AB-testing tooling. Conducted numerous
+                  experiments optimising advertising creatives.
+                </li>
+              </ul>
+            </Typography>
           </Box>
           <Divider />
           <Box>
-            <Typography variant="h6" component="h3">
+            <Typography variant="h5" component="h3">
               Defence Science and Technology Laboratory: Software Engineer
               (intern)
             </Typography>
-            <Typography variant="subtitle1">
+            <Typography variant="subtitle1" component="span">
               August 2010 to July 2011
             </Typography>
-            <p>
-              <Typography variant="body1">
+            <Typography variant="body1">
+              <p>
                 Work included C#, Java, Python, Haskell, Evolutionary
                 Computation, Trusted Computing, analysis of “Big Data”, creating
                 tooling to understand program disassembles, and missing code
                 prediction.
-              </Typography>
-            </p>
+              </p>
+            </Typography>
           </Box>
           <Divider />
           <Box>
-            <Typography variant="h6" component="h3">
+            <Typography variant="h5" component="h3">
               University of Strathclyde: Research Intern
             </Typography>
-            <Typography variant="subtitle1">Summer 2009</Typography>
-            <p>
-              <Typography variant="body1">
+            <Typography variant="subtitle1" component="span">
+              Summer 2009
+            </Typography>
+            <Typography variant="body1">
+              <p>
                 Worked on “Capturing Task Related Knowledge.” I worked
                 independently to investigate ways to identify relevant resources
                 to those currently in-use.
-              </Typography>
-            </p>
+              </p>
+            </Typography>
           </Box>
         </CardContent>
       </Card>
 
       <Card>
         <CardContent>
-          <Typography variant="h5" component="h2">
+          <Typography variant="h4" component="h2">
             Education
           </Typography>
           <Box>
@@ -206,23 +287,23 @@ function App() {
               University of Strathclyde: BSc Software Engineering (Hons 1st
               class)
             </Typography>
-            <Typography variant="subtitle1">2007 - 2012</Typography>
+            <Typography variant="subtitle1" component="span">
+              2007 - 2012
+            </Typography>
 
-            <p>
-              <Typography variant="body1">
+            <Typography variant="body1" paragraph>
+              <p>
                 Demonstrated my coursework to university applicants at open days
                 in 2nd, 3rd and 4th years, and was a lab demonstrator for 1st
                 and 3rd year classes.
-              </Typography>
-            </p>
-            <p>
-              <Typography variant="body1">
+              </p>
+              <p>
                 Awards: Young Software Engineer of the Year (Scotland) – Best
                 Engineered Project, Charles Babbage prize for best honours
                 project, Sword Ciboodle prize runner-up for best project, Dean’s
                 List for applicable years of study (2007, 2008, 2009).
-              </Typography>
-            </p>
+              </p>
+            </Typography>
           </Box>
         </CardContent>
       </Card>
